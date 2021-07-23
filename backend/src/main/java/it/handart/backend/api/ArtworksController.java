0d0ba27ce93a54ee978ed57323a122fd2e4037a8
Francesco <38076434+FrancescoPennacchia@users.noreply.github.com>
2021-07-23 16:51:22 +0200

    Update ArtworksController.java

--- backend/src/main/java/it/handart/backend/api/ArtworksController.java
+++ backend/src/main/java/it/handart/backend/api/ArtworksController.java
@@ -1,15 +1,18 @@
 package it.handart.backend.api;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import it.handart.backend.domain.response.ArtworkResponse;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.URL;
+import java.util.Collections;
 
 @RestController
 @RequestMapping("/api")
@@ -19,20 +22,21 @@ public class ArtworksController {
     private String Token;
 
     @RequestMapping("/artwork")
-    public ResponseEntity<String> getArtwork() {
+    public String getArtwork() throws JsonProcessingException {
 
             RestTemplate restTemplate = new RestTemplate(); //1
             HttpHeaders headers = new HttpHeaders();
+            headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
             headers.add("X-XAPP-Token", Token);
+
             HttpEntity<String> entity = new HttpEntity<>("body", headers);
 
-            System.out.println(entity);
 
             String url = "https://api.artsy.net/api/artworks?size=1"; //2
-            ResponseEntity<String> response
-                    = restTemplate.postForEntity(url, entity,  String.class); //3
+            String response
+                    = restTemplate.postForObject(url, entity,  String.class); //3
 
-System.out.println(response);
+            System.out.println(response);
         return  response;
     }
 
