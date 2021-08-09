9cff60ae0a7878adebba1bf41f34871596ff7218
Francesco <38076434+FrancescoPennacchia@users.noreply.github.com>
2021-08-09 11:25:40 +0200

    up- add lib graph

--- backend/src/main/java/it/handart/backend/api/GraphQLController.java
+++ backend/src/main/java/it/handart/backend/api/GraphQLController.java
@@ -1,9 +1,14 @@
 package it.handart.backend.api;
 
+import io.aexp.nodes.graphql.Argument;
+import io.aexp.nodes.graphql.Arguments;
+import io.aexp.nodes.graphql.GraphQLRequestEntity;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.MalformedURLException;
 
 @RestController
 @RequestMapping("/api/graph")
@@ -17,7 +22,13 @@ public class GraphQLController {
 
     /* Richiesta per singolo Artwork e Artist*/
     @RequestMapping("/artwork/{idArtwork}")
-    public void getArtworkAndArtistById(@PathVariable String idArtwork) {
+    public void getArtworkAndArtistById(@PathVariable String idArtwork) throws MalformedURLException {
+        GraphQLRequestEntity requestEntity = GraphQLRequestEntity.Builder()
+                .url(url_graphql)
+                .scalars(Date.class)
+                .arguments(new Arguments("getThreads", new Argument<>("input", "argument")))
+                .request(ThreadResult.class)
+                .build();
 
     }
 }
