d3c2359cd8e142c4fa05bbdc22fff8abeb9a8df9
Francesco <38076434+FrancescoPennacchia@users.noreply.github.com>
2021-08-23 22:30:49 +0200

    Update GraphQLController.java

--- backend/src/main/java/it/handart/backend/api/GraphQLController.java
+++ backend/src/main/java/it/handart/backend/api/GraphQLController.java
@@ -3,6 +3,7 @@ package it.handart.backend.api;
 import io.aexp.nodes.graphql.*;
 import io.aexp.nodes.graphql.exceptions.GraphQLException;
 import io.aexp.nodes.graphql.internal.Error;
+import it.handart.backend.domain.graph.artist.Artist;
 import it.handart.backend.domain.graph.artwork.Artwork;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -50,27 +51,28 @@ public class GraphQLController {
 
     /* Richiesta Artisti popolari */
     @RequestMapping("/popular_artists")
-    public Artwork getPopularArtist(@RequestParam int size) throws MalformedURLException {
+    public List<Artist> getPopularArtist(@RequestParam int size) throws MalformedURLException {
 
         Map<String, String> headers = new HashMap<>();
         headers.put("X-XAPP-Token", Token);
 
+        List<Artist> result = new ArrayList<>();
+
         GraphQLTemplate graphQLTemplate = new GraphQLTemplate();
+        try {
+            GraphQLRequestEntity requestEntity = GraphQLRequestEntity.Builder()
+                    .url(url_graphql)
+                    .headers(headers)
+                    .arguments(new Arguments("popular_artists", new Argument<>("size", size)))
+                    .request(Artist.class)
+                    .build();
 
-        GraphQLRequestEntity requestEntity = GraphQLRequestEntity.Builder()
-                .url(url_graphql)
-                .headers(headers)
-                .arguments(new Arguments("popular_artists", new Argument<>("size", size)))
-                .request(Artwork.class)
-                .build();
-
-        GraphQLResponseEntity<Artwork> responseEntity = graphQLTemplate.query(requestEntity, Artwork.class);
-
-        if (responseEntity.getErrors() != null && responseEntity.getErrors().length > 0) {
-            Error error = responseEntity.getErrors()[0];
-            throw new GraphQLException(error.getMessage());
+            GraphQLResponseEntity<Artist> responseEntity = graphQLTemplate.query(requestEntity, Artist.class);
+            result = responseEntity.getResponse();
+        } catch (Exception e){
+            e.printStackTrace();
         }
-        return responseEntity.getResponse();
+        return result;
     }
 
 }
